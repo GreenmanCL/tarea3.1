4d2c3c49d6de342e6f43c6cb16c76b473e8e7ad9
GreenmanCL <darwinclhe@gmail.com>
2025-11-21 19:21:01 -0600

    Segunda version, comentarios

--- problemaomegaup.cpp
+++ problemaomegaup.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int32_t main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
 
-    int n;
-    cin >> n;
-    int ans = 0, t;
-    for( int i = 0; i < n; i++ ) {
-        cin >> t;
-        ans = (t == 8) ? 1 : ans;
+    int n; // declaramos n
+    cin >> n;// leemos n
+    int ans = 0, t; // declaramos la variable t y la bandera
+    for( int i = 0; i < n; i++ ) { // hacemos el for
+        cin >> t; // leemos t
+        ans = (t == 8) ? 1 : ans; // comprobamos
     }
-    cout << ans;
+    cout << ans; // imprimimos la respuesta
 
     return 0;
 }
